a8335651247cbc16857e0d92fed877ab9e37f0ae
Pooja Dhoot <pooja@trustingsocial.com>
2019-09-10 14:21:35 +0530

    Testing 1 2 3 4 5 67

--- check_file_changed.sh
+++ check_file_changed.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-check_file_changed()
-{
-        pwd
-	echo "In the script"	
+check_file_changed() {
 	if $(git whatchanged -n 1 | grep -q "$1");
 	then
 		cp "nomad_jobs/$1" ".travisci/upload_activation_s3/$(cut -d"." -f1<<<$1)_`date -I`_$TRAVIS_BUILD_NUMBER.$(cut -d"." -f2<<<$1)"
-		echo "Coding in echo"
-		return 0;
+		return 100;
 	else
-		return 1;
+		return 99;
 
-#	if $(git whatchanged -n 1 | grep -q "$1") ;
-#		pwd
-#		echo "Life in Pune"
-#		#cp "../nomad_jobs/$1" ".travisci/upload_activation_s3/$(cut -d"." -f1<<<$1)_`date -I`_$TRAVIS_BUILD_NUMBER.$(cut -d"." -f2<<<$1)"
-#		echo "Code is in if loop"
-#		then return 0; 
-#	else 
-#		echo "Code is in else loop"
-#		print "Code is in print loop"
-#		return 1; 
 	fi	
 }
 
 check_file_changed "$@"
 echo $?
-
--- nomad_jobs/a.sh
+++ nomad_jobs/a.sh
@@ -4,4 +4,25 @@ This file is working. This file should be copied
 This file is working. This file should be copie
 adasdasdasd
 d
-adasdasdasdasdsdasdasdasddasdasdasdasdasdasddadsasdasadasdasddsasdasdasdasdd
+adasdasdasdasdsdasdasdasddasdasdasdasdasdasddadsasdasadasdasddsasdasdasdasdsi
+
+
+
+
+adadThis file is working. This file should be copied
+This file is working. This file should be copied
+This file is working. This file should be copied
+This file is working. This file should be copie
+This file is working. This file should be copied
+This file is working. This file should be copied
+This file is working. This file should be copied
+This file is working. This file should be copie
+This file is working. This file should be copied
+This file is working. This file should be copied
+This file is working. This file should be copied
+This file is working. This file should be copie
+This file is working. This file should be copied
+This file is working. This file should be copied
+This file is working. This file should be copied
+This file is working. This file should be copie
+d
