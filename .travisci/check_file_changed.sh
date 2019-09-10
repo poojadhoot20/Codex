326191e61eed49457ffc83f51b65ab76ebb2f82c
Pooja Dhoot <pooja@trustingsocial.com>
2019-09-10 10:11:45 +0530

    Testing 1

--- .travisci/check_file_changed.sh
+++ .travisci/check_file_changed.sh
@@ -1,7 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 check_file_changed()
-{ if echo "$(git diff --name-only $TRAVIS_COMMIT_RANGE)" | grep -q "$1" ; then return 0; else return 1; fi	}
+{ 
+	if $(git diff --name-only $TRAVIS_COMMIT_RANGE | grep -q "$1") ;
+		cp "nomad_jobs/a.sh" ".travisci/upload_activation_s3/$(cut -d"." -f1<<<$1)_`date -I`_$TRAVIS_BUILD_NUMBER.$(cut -d"." -f2<<<$1)"
+		then return 0; 
+	else 
+		return 1; 
+	fi	
+}
 
-check_file_changed
+check_file_changed "$@"
 echo $?
+
