830c0c277108dc30fd72a2971220ecd01bd0158a
Pooja Dhoot <pooja@trustingsocial.com>
2019-09-13 15:31:52 +0530

    First commiut

--- check_file_changed.sh
+++ check_file_changed.sh
@@ -4,8 +4,6 @@ check_file_changed() {
 	if $(git whatchanged -n 1 | grep -q "$1");
 	then
 		cp "nomad_jobs/$1" ".travisci/upload_activation_s3/$(cut -d"." -f1<<<$1)_`date -I`_$TRAVIS_BUILD_NUMBER.$(cut -d"." -f2<<<$1)"
-		echo "PWD will be seen"
-		pwd
 		return 100;
 	else
 		return 99;
